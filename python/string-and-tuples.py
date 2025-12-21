8a3bbc0c73bf35af63a00d8c3c3e268f2f484553
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-12-21 11:04:46 +0100

    update: add strings and tuples

---
+++ python/string-and-tuples.py
@@ -0,0 +1,19 @@
+# strings 
+
+name = "Zerod"
+firstLetter = name[0]
+
+print(firstLetter)
+
+
+content = ("axe", 34, name)
+print(content)
+# tuple can be converted to and from list using tuple, list
+
+contentList = list(content)
+
+print(contentList)
+
+
+contentTuple = tuple(contentList)
+print(contentList)
