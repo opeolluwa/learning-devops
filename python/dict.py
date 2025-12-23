612d948ebec93ea394fc875a5908bf995c3b13f3
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-12-23 21:59:47 +0100

    update:

---
+++ python/dict.py
@@ -0,0 +1,9 @@
+myGrades = {"biology": "A+", "english": "C4", "chemistry": "f"}
+
+print(myGrades["biology"])
+
+# use keys, items and value like you do in javascript
+
+# use item in dict.keys() to see if a key exist or similar for a value
+
+# two objects can be compared {'alex':3} == {'alex':3} this is nit true for array (or list as Python call them)
